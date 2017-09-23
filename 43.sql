92daf5a105c5a99ae368b2a0e18cfd1b64027890
Dan Hoover <mudmin@gmail.com>
2017-09-23 14:40:27 -0800

    UserSpice Alpha 4.3.022

--- 43.sql
+++ 43.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2017 at 09:42 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Sep 24, 2017 at 12:40 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -178,7 +178,13 @@ INSERT INTO `logs` (`id`, `user_id`, `logdate`, `logtype`, `lognote`, `added`) V
 (27, 1, '2017-09-24 03:27:50', 'Email Settings', 'Updated isSMTP from 1 to 0.', '2017-09-23 11:27:50'),
 (28, 1, '2017-09-24 03:27:50', 'Email Settings', 'Updated isHTML from false to true.', '2017-09-23 11:27:50'),
 (29, 1, '2017-09-24 03:27:50', 'Email Settings', 'Updated useSMTPauth from false to true.', '2017-09-23 11:27:50'),
-(30, 1, '2017-09-24 03:30:47', 'Email Settings', 'Updated email_act from 1 to 0.', '2017-09-23 11:30:47');
+(30, 1, '2017-09-24 03:30:47', 'Email Settings', 'Updated email_act from 1 to 0.', '2017-09-23 11:30:47'),
+(31, 1, '2017-09-24 05:31:07', 'Admin Verification', 'Access granted to users/admin_users.php via password verification.', '2017-09-23 13:31:07'),
+(32, 1, '2017-09-24 05:38:57', 'User Manager', 'Added user test.', '2017-09-23 13:38:57'),
+(33, 1, '2017-09-24 05:45:45', 'User', 'User logged in.', '2017-09-23 13:45:45'),
+(34, 1, '2017-09-24 06:22:37', 'User', 'User logged in.', '2017-09-23 14:22:37'),
+(35, 1, '2017-09-24 06:26:18', 'User Manager', 'Added user test67.', '2017-09-23 14:26:18'),
+(36, 1, '2017-09-24 06:33:01', 'User', 'Changed fname from Dan to Dan2.', '2017-09-23 14:33:01');
 
 -- --------------------------------------------------------
 
@@ -496,6 +502,7 @@ CREATE TABLE `settings` (
   `auto_assign_un` int(1) NOT NULL DEFAULT '0',
   `page_permission_restriction` int(1) NOT NULL DEFAULT '0',
   `msg_blocked_users` int(1) NOT NULL DEFAULT '0',
+  `msg_default_to` int(1) NOT NULL DEFAULT '1',
   `notif_daylimit` int(3) NOT NULL DEFAULT '7',
   `recap_public` varchar(100) NOT NULL,
   `recap_private` varchar(100) NOT NULL
@@ -505,8 +512,8 @@ CREATE TABLE `settings` (
 -- Dumping data for table `settings`
 --
 
-INSERT INTO `settings` (`id`, `recaptcha`, `force_ssl`, `css_sample`, `us_css1`, `us_css2`, `us_css3`, `site_name`, `language`, `track_guest`, `site_offline`, `force_pr`, `glogin`, `fblogin`, `gid`, `gsecret`, `gredirect`, `ghome`, `fbid`, `fbsecret`, `fbcallback`, `graph_ver`, `finalredir`, `req_cap`, `req_num`, `min_pw`, `max_pw`, `min_un`, `max_un`, `messaging`, `snooping`, `echouser`, `wys`, `change_un`, `backup_dest`, `backup_source`, `backup_table`, `msg_notification`, `permission_restriction`, `auto_assign_un`, `page_permission_restriction`, `msg_blocked_users`, `notif_daylimit`, `recap_public`, `recap_private`) VALUES
-(1, 1, 0, 0, '../users/css/color_schemes/bootstrap.min.css', '../users/css/sb-admin.css', '../users/css/custom.css', 'UserSpice', 'en', 1, 0, 0, 1, 0, '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, '/', 'everything', '', 0, 0, 0, 0, 0, 7, '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
+INSERT INTO `settings` (`id`, `recaptcha`, `force_ssl`, `css_sample`, `us_css1`, `us_css2`, `us_css3`, `site_name`, `language`, `track_guest`, `site_offline`, `force_pr`, `glogin`, `fblogin`, `gid`, `gsecret`, `gredirect`, `ghome`, `fbid`, `fbsecret`, `fbcallback`, `graph_ver`, `finalredir`, `req_cap`, `req_num`, `min_pw`, `max_pw`, `min_un`, `max_un`, `messaging`, `snooping`, `echouser`, `wys`, `change_un`, `backup_dest`, `backup_source`, `backup_table`, `msg_notification`, `permission_restriction`, `auto_assign_un`, `page_permission_restriction`, `msg_blocked_users`, `msg_default_to`, `notif_daylimit`, `recap_public`, `recap_private`) VALUES
+(1, 1, 0, 0, '../users/css/color_schemes/bootstrap.min.css', '../users/css/sb-admin.css', '../users/css/custom.css', 'UserSpice', 'en', 1, 0, 0, 0, 0, '', '', '', '', '', '', '', '', '', 0, 0, 6, 30, 4, 30, 1, 1, 0, 1, 0, '/', 'everything', '', 0, 0, 0, 0, 0, 1, 7, '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
 
 -- --------------------------------------------------------
 
@@ -554,7 +561,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `fname`, `lname`, `permissions`, `logins`, `account_owner`, `account_id`, `company`, `join_date`, `last_login`, `email_verified`, `vericode`, `active`, `oauth_provider`, `oauth_uid`, `gender`, `locale`, `gpluslink`, `picture`, `created`, `modified`, `fb_uid`, `un_changed`, `msg_exempt`, `last_confirm`, `protected`, `dev_user`, `msg_notification`, `force_pr`) VALUES
-(1, 'userspicephp@gmail.com', 'admin', '$2y$12$1v06jm2KMOXuuo3qP7erTuTIJFOnzhpds1Moa8BadnUUeX0RV3ex.', 'Dan', 'Hoover', 1, 53, 1, 0, 'UserSpice', '2016-01-01 00:00:00', '2017-09-23 18:46:46', 1, '322418', 0, '', '', '', '', '', '', '0000-00-00 00:00:00', '1899-11-30 00:00:00', '', 0, 1, '2017-09-20 01:44:44', 0, 0, 1, 0),
+(1, 'userspicephp@gmail.com', 'admin', '$2y$12$1v06jm2KMOXuuo3qP7erTuTIJFOnzhpds1Moa8BadnUUeX0RV3ex.', 'Dan2', 'Hoover', 1, 55, 1, 0, 'UserSpice', '2016-01-01 00:00:00', '2017-09-23 22:22:37', 1, '322418', 0, '', '', '', '', '', '', '0000-00-00 00:00:00', '1899-11-30 00:00:00', '', 0, 1, '2017-09-23 22:26:21', 0, 0, 1, 0),
 (2, 'noreply@userspice.com', 'user', '$2y$12$HZa0/d7evKvuHO8I3U8Ff.pOjJqsGTZqlX8qURratzP./EvWetbkK', 'Sample', 'User', 1, 6, 1, 0, 'none', '2016-01-02 00:00:00', '2017-09-09 15:10:46', 1, '970748', 1, '', '', '', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0, 0, NULL, 0, 0, 1, 0);
 
 -- --------------------------------------------------------
@@ -576,7 +583,7 @@ CREATE TABLE `users_online` (
 --
 
 INSERT INTO `users_online` (`id`, `ip`, `timestamp`, `user_id`, `session`) VALUES
-(1, '::1', '1506195416', 1, '');
+(1, '::1', '1506206098', 1, '');
 
 -- --------------------------------------------------------
 
@@ -771,7 +778,7 @@ ALTER TABLE `keys`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 --
 -- AUTO_INCREMENT for table `logs_exempt`
 --
@@ -816,7 +823,7 @@ ALTER TABLE `permission_page_matches`
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `settings`
 --
@@ -826,7 +833,7 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `users_online`
 --
@@ -841,7 +848,7 @@ ALTER TABLE `users_session`
 -- AUTO_INCREMENT for table `user_permission_matches`
 --
 ALTER TABLE `user_permission_matches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
