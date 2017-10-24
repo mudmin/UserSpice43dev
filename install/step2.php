579793775bc55fd294f3df270fdcadfd83f814f6
Dan Hoover <mudmin@gmail.com>
2017-10-24 12:57:50 -0800

    new build

--- install/step2.php
+++ install/step2.php
@@ -175,7 +175,7 @@ fclose($fh);
 $fh=fopen($config_file , "a+");
 $end = "';";
 fwrite($fh , $timezone_syn . $tz . $end . PHP_EOL);
-fwrite($fh, "date_default_timezone_set($timezone_string)" . $end . PHP_EOL);
+fwrite($fh, "date_default_timezone_set($timezone_string);" . PHP_EOL);
 fclose($fh);
 redirect("step3.php")
 ?>
