eece03053a8edc5aeaade223a681ee484715b009
mudmin@gmail.com <mudmin@gmail.com>
2018-02-07 05:14:01 -0900

    UserSpice 4.3.13 installer update

--- users/update.php
+++ users/update.php
@@ -2,7 +2,7 @@
 require_once 'init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/header.php';
 require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
-if (!securePage($_SERVER['PHP_SELF'])){die();}
+//if (!securePage($_SERVER['PHP_SELF'])){die();}
 $count = 0;
 $updates = $db->query("SELECT * FROM updates")->results();
 $existing_updates=[];
