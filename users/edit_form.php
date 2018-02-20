57eab339a87485e3a7a28413f9e9e7951b80e5a3
Dan Hoover <mudmin@gmail.com>
2018-02-20 14:48:56 -0900

    edit_form.php hotfix

--- users/edit_form.php
+++ users/edit_form.php
@@ -1,10 +1,9 @@
 <?php
-
 require_once 'init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/header.php';
 require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
 
-if(in_array(!$user->data()->id,$master_account)){die();}
+if(!in_array($user->data()->id,$master_account)){die();}
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 //Errors Successes
 $errors = [];
